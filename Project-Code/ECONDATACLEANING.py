5aa17e1ee49253f45f06026f2cd78e0bfef08a6a
Eliot Ozaki <eliot.ozaki@smetters.net>
2024-07-28 16:07:40 -0700

    Added both the County Gdp effect dataset (CAGDP11) and cleaned some of the GDP total dataset by adding cleaning lines to the ECONDATACLEANING.py file.

--- Project-Code/ECONDATACLEANING.py
+++ Project-Code/ECONDATACLEANING.py
@@ -68,10 +68,10 @@ initials = list(state_initials.values())
 population_df.tail(20)
 
 for state in states:
-    population_df.loc[df['State']==state,'State'] = state_initials[state]
+    population_df.loc[population_df['State']==state,'State'] = state_initials[state]
     
 print(population_df['State'].unique())
-print(population_df['County'][df['County'].apply(len) <= 2])
+print(population_df['County'][population_df['County'].apply(len) <= 2])
 
 index = 0
 popcounts = 0
@@ -149,25 +149,46 @@ import seaborn as sns
 import calmap
 #from pandas_profiling import ProfileReport
 
-population_df = pd.read_csv("C:/Users/eliot/OneDrive/Desktop/ECONPROJECT/Cleaned-Datasets/POPULATIONDATA-Cleaned.csv")
-emissions_df = pd.read_csv("C:/Users/eliot/OneDrive/Desktop/ECONPROJECT/Base-Datasets/COUNTY_EMISSIONS2021.csv")
+population_df = pd.read_csv("Cleaned-Datasets\POPULATIONDATA-Cleaned.csv")
+emissions_df = pd.read_csv("Cleaned-Datasets/EMISSIONSDATA-Cleaned.csv")
 merged_df = pd.read_csv("AllData-MergedDS.csv")
 
 import chardet
-with open("C:/Users/eliot/OneDrive/Desktop/ECONPROJECT/Cleaned-Datasets/County-MSA-GDP-DATA.csv", 'rb') as rawdata:
+with open("Cleaned-Datasets\County-MSA-GDP-DATA.csv", 'rb') as rawdata:
     result = chardet.detect(rawdata.read(100000))
 result
 
-gdp_df = pd.read_csv("C:/Users/eliot/OneDrive/Desktop/ECONPROJECT/Cleaned-Datasets/County-MSA-GDP-DATA.csv",encoding="Windows-1252")
+gdp_df = pd.read_csv("Cleaned-Datasets\County-MSA-GDP-DATA.csv",encoding="Windows-1252")
 gdp_df.describe()
 
 gdp_df.head(10)
 
+gdp_df = gdp_df[~gdp_df['GeoName'].isin(states + ['United States'])]
 
+print(len(gdp_df)/3)
+counties_not_in_gdp = set(merged_df['County'].str.split(',').str[0]) - set(gdp_df['GeoName'].str.split(',').str[0])
+print("Counties in merged_df but not in gdp_df:")
+for county in counties_not_in_gdp:
+    print(county)
 
+counties_not_in_merged = set(gdp_df['GeoName'].str.split(',').str[0]) - set(merged_df['County'].str.split(',').str[0])
+print("Counties in gdp but not in merged:")
+for county in counties_not_in_merged:
+    print(county)
 
+gdp_df.loc[gdp_df['GeoName'].str.contains(r' \(Independent City\)', regex=True), 'GeoName'] = \
+    gdp_df['GeoName'].str.replace(r' \(Independent City\)', '', regex=True).str.strip().apply(lambda x: "City of " + x)
+gdp_df['GeoName'] = gdp_df['GeoName'].str.replace('St.', 'St')
+gdp_df['GeoName'] = gdp_df['GeoName'].str.replace('East', 'E')
+gdp_df['GeoName'] = gdp_df['GeoName'].str.replace('Census Area', '')
+gdp_df['GeoName'] = gdp_df['GeoName'].str.replace('City and Borough', '')
+gdp_df['GeoName'] = gdp_df['GeoName'].str.replace('Borough', '')
+gdp_df.loc[gdp_df['GeoName'] =='LaSalle', 'GeoName'] = 'La Salle'
 
-
+print(gdp_df.loc[gdp_df['GeoName'].str.contains('Emporia'),'GeoName'])
+print(merged_df.loc[merged_df['County'].str.contains('Columbia'),'County'])
+print(gdp_df.loc[~gdp_df['GeoName'].str.contains(','),'GeoName'])
+gdp_df.set_index("GeoName")
 
 
 
